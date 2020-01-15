680764b327ba1f80e9d841a4435a57a103939f45
Nick <55280957+NAEK2K@users.noreply.github.com>
2020-01-15 18:06:39 -0500

    game file

---
+++ walk.cc
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <stdlib.h>
+
+class Game {
+    public:
+
+        // constructor - sets the maps initial values
+        Game() {
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    map[i][j] = 0;
+                }
+            }
+            x = 0;
+            y = 0;
+        }
+        // displays the map in the terminal
+        void show() {
+            system("clear");
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
+                    if(this->x == j && this->y == i) {
+                        std::cout << 1 << " ";
+                    } else {
+                        std::cout << map[i][j] << " ";
+                    }
+                }
+                std::cout << std::endl;
+            }
+        }
+        // checks if x or y will go out of bounds in the next move, returns true if it will go out of bounds
+        bool edgeCheck(int x, int y) {
+            return this->x + x > 9 || this->x + x < 0 || this->y + y > 9 || this->y + y < 0;
+        }
+
+        // move the "character" around the map
+        void move(char dir) {
+            if(dir == 'w') {
+                if(!edgeCheck(0, -1)) {
+                        this->y--;
+                    }
+            } else if(dir == 's') {
+                if(!edgeCheck(0, 1)) {
+                        this->y++;
+                    }
+            } else if(dir == 'a') {
+                if(!edgeCheck(-1, 0)) {
+                        this->x--;
+                    }
+            } else if(dir == 'd') {
+                if(!edgeCheck(1, 0)) {
+                        this->x++;
+                    }
+            }
+        }
+
+        // runs the game in the terminal window
+        void run() {
+            this->show();
+            char move;
+            while (move != 'q') {
+                std::cin >> move;
+                // exit command
+                if (move == 'q') {
+                    break;
+                }
+                else {
+                    this->move(move);
+                }
+                this->show();
+            }
+        }
+
+    private:
+        int x, y;
+        int map[10][10];
+};
+
+int main() {
+    Game game;
+    game.run();
+    
+    return 0;
+}
